0e116527d99107b0257f5dc722526612027ad691
BrittSH <89482952+BrittSH@users.noreply.github.com>
2022-10-05 18:45:36 -0500

    Calling a Di'd service

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private pizzaSvc: PizzaService
+  ) { }
 
   ngOnInit(): void {
+    const pt = this.pizzaSvc.getPizzaToppingsFromTheCloud();
+    console.log(pt);
   }
 
 }
--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() { 
+    const toppingsFromWebService = [
+      {
+        name: 'Extra Cheese'
+        , price: 1.00
+      }
+      , {
+        name: 'Mushrooms'
+        , price: .50
+      }
+      , {
+        name: 'Bacon'
+        , price: 2.50
+      }
+    ];
+    return toppingsFromWebService;
+  }
 }
