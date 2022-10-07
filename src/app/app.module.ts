58e0b5d87cb029472051b9ffec8ba2e34b49eaa0
skylarNahn <115182635+skylarNahn@users.noreply.github.com>
2022-10-07 14:49:42 -0500

    We need bidirectional binding for checkboxes
    I always forget this but remembered this time

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
