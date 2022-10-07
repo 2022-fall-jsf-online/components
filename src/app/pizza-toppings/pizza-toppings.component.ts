e6825f8fab1af1c139448276d332467032ba1168
Rashad Adams <96805229+Rashad-636@users.noreply.github.com>
2022-10-07 15:46:39 -0400

    Added PizzaToppingsDisplay interface ..
    And transformed the data to be the proper shape (added checked to object literal)

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+interface PizzaToppingsDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -8,15 +14,29 @@ import { PizzaService } from '../pizza.service';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-
   // Magic dependacy injection...
   constructor(
     private pizzaSvc: PizzaService
   ) { }
-  ngOnInit(): void {
 
+    availablePizzaToppings: PizzaToppingsDisplay[ ]= [ ];
+
+    ngOnInit(): void {
+    
+      // creates variabkles that calls function from the service page
     const pizzaToppings = this.pizzaSvc.getPizzaToppingsFromCloud();
     console.log(pizzaToppings);
+
+    // adds checked to the array object
+    this.availablePizzaToppings = pizzaToppings.map(
+      x => ({
+        ...x,
+        checked: false
+      })
+    );
+
+    console.log(this.availablePizzaToppings);
+    
     
   }
 
