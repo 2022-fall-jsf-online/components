1f81870f6d987a965a35f1892bc70d5341e236db
Rashad Adams <96805229+Rashad-636@users.noreply.github.com>
2022-10-07 15:27:30 -0400

    Boom!! We're calling a DI's service!!
    This is fun, lets show the pizza toppings in the component ..

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PizzaService } from '../pizza.service';
 
 @Component({
   selector: 'app-pizza-toppings',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PizzaToppingsComponent implements OnInit {
 
-  constructor() { }
 
+  // Magic dependacy injection...
+  constructor(
+    private pizzaSvc: PizzaService
+  ) { }
   ngOnInit(): void {
+
+    const pizzaToppings = this.pizzaSvc.getPizzaToppingsFromCloud();
+    console.log(pizzaToppings);
+    
   }
 
 }
