c89de3fd5daa3499a38346949372ad4d681520d2
DrewHalverson <67759475+DrewHalverson@users.noreply.github.com>
2022-10-09 14:39:59 -0500

    We're calling a dependency injected service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
-}
+
+  getPizzaToppingsFromTheCloud() {
+    //pretend we make a web service call
+
+    const toppingsFromWebService = [
+      {
+        name: 'Olives'
+        , price: 2.25
+      }
+      ,      {
+        name: 'Peppers'
+        , price: 1.50
+      }
+      ,      {
+        name: 'Mushrooms'
+        , price: 1.75
+      }
+    ];
+
+    return toppingsFromWebService;
+    }
+  }
+
