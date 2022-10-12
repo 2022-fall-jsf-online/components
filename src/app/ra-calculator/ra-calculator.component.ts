2be855afed32949e4a4715d59a695040e1e782ad
Rashad Adams <96805229+Rashad-636@users.noreply.github.com>
2022-10-12 18:19:40 -0400

    Slack n tell finished!
    Added an addition, subtraction, multiply and divide buttons. Then added functions so that the appropriate total is displayed with message.

--- src/app/ra-calculator/ra-calculator.component.ts
+++ src/app/ra-calculator/ra-calculator.component.ts
@@ -12,4 +12,21 @@ export class RaCalculatorComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // initializes variables
+  numberOne = 0;
+  numberTwo = 0;
+  total = " " + 0;
+
+  // Function adds both input numbers
+  add = () => this.total = (" Your total is ") + (this.numberOne + this.numberTwo);
+
+  // Function subtracts both input numbers
+  subtract = () => this.total = " Your total is " + (this.numberOne - this.numberTwo);
+ 
+  // Function multiplies both input numbers
+  multiply = () => this.total = " Your total is " + (this.numberOne * this.numberTwo);
+
+  // Function divides both input numbers
+  divide = () => this.total = " Your total is " + (this.numberOne / this.numberTwo);
+  
 }
