6b8e5877d1bfc5c6eedfbec067bb126c3dd49c3c
EmeraldPangolin <114652722+EmeraldPangolin@users.noreply.github.com>
2022-10-15 04:05:51 -0500

    Generated a pizza toppings component
    used this to generate new component....
    
    ng generate component pizza-toppings

---
+++ src/app/pizza-toppings/pizza-toppings.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PizzaToppingsComponent } from './pizza-toppings.component';
+
+describe('PizzaToppingsComponent', () => {
+  let component: PizzaToppingsComponent;
+  let fixture: ComponentFixture<PizzaToppingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PizzaToppingsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PizzaToppingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
