a4c79c63e8b7107497600aaa2e14d256385aa2bf
EmeraldPangolin <114652722+EmeraldPangolin@users.noreply.github.com>
2022-10-16 03:10:09 -0500

    made my component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { DmeyMultiplyComponent } from './dmey-multiply/dmey-multiply.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    DmeyMultiplyComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dmey-multiply/dmey-multiply.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dmey-multiply',
+  templateUrl: './dmey-multiply.component.html',
+  styleUrls: ['./dmey-multiply.component.css']
+})
+export class DmeyMultiplyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
