7b4eb6234cfe45db19f39a11e55fc67e5fa8cc17
EmeraldPangolin <114652722+EmeraldPangolin@users.noreply.github.com>
2022-10-16 03:15:20 -0500

    made imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { DmeyMultiplyComponent } from './dmey-multiply/dmey-multiply.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 @NgModule({
   declarations: [
@@ -25,6 +29,8 @@ import { DmeyMultiplyComponent } from './dmey-multiply/dmey-multiply.component';
     , MatCardModule
     , MatButtonModule
     , MatCheckboxModule
+    , MatInputModule
+    , MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
