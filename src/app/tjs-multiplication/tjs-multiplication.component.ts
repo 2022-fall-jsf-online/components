2e108bbd7ce0a295798a95c04e906a957c0bc3c9
Anna McCreery <114881364+annamccreery@users.noreply.github.com>
2022-10-18 13:55:54 -0500

    Reverted back due to bug
    Stop trying to skip ahead and just learn along with the video

--- src/app/tjs-multiplication/tjs-multiplication.component.ts
+++ src/app/tjs-multiplication/tjs-multiplication.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-alm-multiplication',
-  templateUrl: './alm-multiplication.component.html',
-  styleUrls: ['./alm-multiplication.component.css']
+  selector: 'app-tjs-multiplication',
+  templateUrl: './tjs-multiplication.component.html',
+  styleUrls: ['./tjs-multiplication.component.css']
 })
-export class AlmMultiplicationComponent implements OnInit {
+export class TjsMultiplicationComponent implements OnInit {
 
   constructor() { }
 
