d2f7f1e54a5302cef256b75d2881510d906f4ca8
Anna McCreery <114881364+annamccreery@users.noreply.github.com>
2022-10-18 16:31:34 -0500

    Undoing Work
    Took work toward providing a total, but backtracked as this seemed sufficient.

--- src/app/alm-addition/alm-addition.component.ts
+++ src/app/alm-addition/alm-addition.component.ts
@@ -7,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlmAdditionComponent implements OnInit {
 
-  constructor() { }
+  constructor( ) { }
+
 
   ngOnInit(): void {
-  }
-  
-
+}
 }
