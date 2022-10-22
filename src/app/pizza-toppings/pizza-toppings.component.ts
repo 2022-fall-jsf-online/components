d37e3d0d317c95231a4f6293fc9a56ea1a1dbc1d
skylarNahn <115182635+skylarNahn@users.noreply.github.com>
2022-10-22 17:43:23 -0500

    Added button click functionality. woot

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -47,4 +47,20 @@ export class PizzaToppingsComponent implements OnInit {
       )
     ;
   }
+
+  checkAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      , checked: true
+    })
+  );
+
+  uncheckAll = () => this.availablePizzaToppings = this.availablePizzaToppings.map(
+    x => ({
+      ...x
+      , checked: false
+    })
+  );
+
+
 }
