97041c9154a21d0c27df904eefdc8691a37bacca
Rhymlore <bakirci.alperen@gmail.com>
2022-10-24 19:47:06 -0500

    Created a component called alperen-addition

---
+++ src/app/alperen-addition/alperen-addition.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alperen-addition',
+  templateUrl: './alperen-addition.component.html',
+  styleUrls: ['./alperen-addition.component.css']
+})
+export class AlperenAdditionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
