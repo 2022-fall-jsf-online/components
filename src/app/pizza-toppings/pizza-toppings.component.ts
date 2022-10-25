14074d878935832c68e606116c9a86a4bf666753
Rebecca Lee <rklee@madisoncollege.edu>
2022-10-25 17:39:38 -0500

    Added PizzaToppingsDisplay Interface
    This ensures that the shape is how we want it.

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from '../pizza.service';
 
+
+//Shape for Model/View
+//Ensures correct datatypes are used
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -14,10 +23,26 @@ export class PizzaToppingsComponent implements OnInit {
     private pizzaSvc: PizzaService
   ) { }
 
+  //Using this variable to hold the toppings that are pulled in from API
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
+
   ngOnInit(): void {
     //Calling this instanceVariable to run function
+    //Get objects from API/Service
     const pt = this.pizzaSvc.getPizzaToppingFromTheCloud();
     console.log(pt);
+
+    //Put toppings from API into this Array
+    //Transform them into the shape that we want (from interface above)
+    this.availablePizzaToppings = pt.map(
+      //spread in x (name & price) & add checked property, initialized to false
+      x => ({
+        ...x
+        , checked: false
+      })
+    );
+    console.log(this.availablePizzaToppings);
   }
 
 }
