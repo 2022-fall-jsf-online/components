17a6b5d4e905ffcfed7b23b230fe762caa9a9f45
Raymond Bach <raymondmsbach@gmail.com>
2022-10-29 16:28:47 -0500

    We're calling a DI'd service

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class PizzaService {
 
   constructor() { }
+
+  getPizzaToppingsFromTheCloud() {
+    //preten we make a web service call here
+
+    const toppingsFromWebService = [
+      {
+        name: 'Olives',
+        price: 2.25
+      },
+      {
+        name: 'Pepperoni',
+        price: 1.50
+      },
+      {
+        name: 'Sausage',
+        price: 1.75
+      }
+    ];
+
+    return toppingsFromWebService;
+  }
 }
