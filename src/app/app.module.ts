9b7d5f266b694f4ca7d1e160dcba153a6b44cb2e
David Esteves <rollmop.flosses.0z@icloud.com>
2022-10-30 22:51:05 -0400

    Created new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PizzaToppingsComponent } from './pizza-toppings/pizza-toppings.component';
 
+
+import { DestComponentComponent } from './dest-component/dest-component.component';
+
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -12,10 +15,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    DestComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dest-component/dest-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dest-component',
+  templateUrl: './dest-component.component.html',
+  styleUrls: ['./dest-component.component.css']
+})
+export class DestComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
