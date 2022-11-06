084d5a446ec731cf457be23b1f10a53cd94a6e79
Randi Vinney <89804120+rvinney@users.noreply.github.com>
2022-11-06 09:45:45 -0600

    built out the fields for multiplication component

--- src/app/rv-multiplication/rv-multiplication.component.ts
+++ src/app/rv-multiplication/rv-multiplication.component.ts
@@ -12,4 +12,10 @@ export class RvMultiplicationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  numberOne = 10;
+  numberTwo = 90;
+  total = 900;
+
+  multiply = () => this.total = this.numberOne * this.numberTwo;
+
 }
