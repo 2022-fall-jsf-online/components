c3cb74129455dd0aeb8c1b15e945a6ded907094c
tsteelematc <tsteele@madisoncollege.edu>
2022-11-14 08:51:57 -0600

    Display the appointment message on the card footer...
    No DOM manipulation when using a framework ! ! !

--- src/app/dest-component/dest-component.component.ts
+++ src/app/dest-component/dest-component.component.ts
@@ -25,21 +25,12 @@ export class DestComponentComponent implements OnInit {
     date: new FormControl(new Date())
   });
 
+  displayMessage = "";
+
   //logs to console values of form as json object
   //display formatted message via appendChild
   displayAppointment = () => {
-    const displayMessage = `Hi, ${this.name} your appointment for ${this.date} is booked!`
+    this.displayMessage = `Hi, ${this.name} your appointment for ${this.date} is booked!`
       + ` Updates and reminders will be sent to ${this.email}`;
-
-
-    const data = this.appointmentForm.value;
-    console.log(data);
-    console.log(displayMessage); 
-    this.appointmentForm.reset(); //reset form
-    const span = document.createElement("span");
-    span.appendChild(document.createTextNode(displayMessage));
-    document.body.appendChild(span);
-    return displayMessage;
   };
- 
 }
