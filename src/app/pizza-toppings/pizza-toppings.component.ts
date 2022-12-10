ac80d77543bf4f3cda495c7164d3dc2c766be3a6
spatel2mtc <89953193+spatel2mtc@users.noreply.github.com>
2022-12-10 12:50:33 -0600

    Added new function and total price logic

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -36,6 +36,17 @@ export class PizzaToppingsComponent implements OnInit {
       x => ({...x, checked: false})
     )
     console.log(this.availablePizzaToppings);
+    
   }
+  totalPrice = 0;
+
+  calculateTotal = () => {
+    this.totalPrice = this.availablePizzaToppings
+    .filter(
+      x => x.checked
+    ).reduce(
+      (acc, x) => acc + x.price, 0)
+  }
+
 
 }
