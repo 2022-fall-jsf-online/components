9d6b7f419266ecb42fa5757152e3dca335a35b07
spatel2mtc <89953193+spatel2mtc@users.noreply.github.com>
2022-12-10 16:26:02 -0600

    Finalize the component
    Removed several typos causing many errors. Added an input field with two buttons for functions of squaring and square root. Added a field to reset back to zero.

--- src/app/spatel-operation/spatel-operation.component.ts
+++ src/app/spatel-operation/spatel-operation.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spatel-operation',
   templateUrl: './spatel-operation.component.html',
   styleUrls: ['./spatel-operation.component.css']
 })
-export class SpatelOperationComponent {
+export class SpatelOperationComponent implements OnInit {
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+
+  numberFromField = 0;
+
+  squareThisNumber = () => {
+
+    this.numberFromField = this.numberFromField *this.numberFromField;
+
+  }
+  rootThisNumber = () => {
+    this.numberFromField = Math.sqrt(this.numberFromField);
+  }
+
+  resetThisNumber = () => {
+    this.numberFromField = 0;
+  }
+
 
 }
